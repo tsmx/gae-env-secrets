cc77720b2948d8a4b7562bc01767bdf402fdab2e
tsmx <dev@tsmx.net>
2023-09-04 22:02:14 +0200

    options introdcuced for secret suffix

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const client = new SecretManagerServiceClient();
 
+const defaultSuffix = '_SECRET';
+
+function getOptValue(options, optName, defaultOptValue) {
+    if (options && options[optName]) {
+        return options[optName];
+    }
+    else {
+        return defaultOptValue;
+    }
+}
+
 async function getSecret(secretName) {
     const [secret] = await client.accessSecretVersion({ name: secretName });
     return secret.payload.data.toString();
 }
 
-async function getEnvSecrets() {
-    if(!process.env['GAE_SERVICE'] && !process.env['GAE_RUNTIME']) {
+async function getEnvSecrets(options) {
+    const suffix = getOptValue(options, 'suffix', defaultSuffix);
+    if (!process.env['GAE_SERVICE'] && !process.env['GAE_RUNTIME']) {
         console.log('Not running in GAE. Nothing to do for getEnvSecrets.');
         return;
     }
     console.log('Starting getEnvSecrets');
     for (const [key, value] of Object.entries(process.env)) {
-        if (key === 'SECRET_KEY') {
+        if (key.endsWith(suffix)) {
             console.log(`Retrieving secret for key ${key}`);
             try {
                 const secret = await getSecret(value);
