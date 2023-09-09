4ea27f3509bc62a0661d76b2622e153c09b197ad
tsmx <dev@tsmx.net>
2023-09-09 22:11:51 +0200

    Unit tests improved

--- test/gae-env-secrets.test.js
+++ test/gae-env-secrets.test.js
@@ -13,16 +13,40 @@ jest.mock('@google-cloud/secret-manager', () => {
 
 describe('getEnvSecrets test suite', () => {
 
+    var testOutput = [];
+    const originalConsoleLog = console.log;
+    const testConsoleLog = (output) => { testOutput.push(output); };
+
     beforeEach(() => {
-        process.env['GAE_SERVICE'] = 'x';
-        process.env['GAE_RUNTIME'] = 'x';
-        process.env['TEST_SECRET'] = 'secret-reference';
+        delete process.env['GAE_SERVICE'];
+        delete process.env['GAE_RUNTIME'];
+        delete process.env['TEST_SECRET'];
+        console.log = testConsoleLog;
+        testOutput = [];
     });
 
-    it('test a success ful env var secret retrieval', async () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+        console.log = originalConsoleLog;
+    });
+
+    it('test a successful env var secret retrieval', async () => {
+        process.env['GAE_SERVICE'] = 'x';
+        process.env['GAE_RUNTIME'] = 'x';
+        process.env['TEST_SECRET'] = 'original value';
         await getEnvSecrets();
         expect(process.env['TEST_SECRET']).toEqual('Mock-Secret');
         expect(SecretManagerServiceClient).toBeCalledTimes(1);
+        expect(testOutput.length).toBe(3);
+        expect(testOutput[1].toString().endsWith('TEST_SECRET')).toBeTruthy();
+    });
+
+    it('test a \'nothing-to-do\' beacuse GAE is not detected', async () => {
+        process.env['TEST_SECRET'] = 'original value';
+        await getEnvSecrets();
+        expect(process.env['TEST_SECRET']).toEqual('original value');
+        expect(SecretManagerServiceClient).toBeCalledTimes(0);
+        expect(testOutput.length).toBe(1);
     });
 
 });
